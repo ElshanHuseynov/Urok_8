cbaabfc01c8bd97bc230bc6015d73476d78f5611
Elshan <beka417@yandex.ru>
2023-05-23 14:14:26 +0300

    Задача 58

--- Program.cs
+++ Program.cs
@@ -41,41 +41,94 @@
 // }
 
 // Задача 56
+// class Program
+// {
+//     static void Main()
+//     {
+//         int[,] array = 
+//         {
+//             { 1, 4, 7, 2 },
+//             { 5, 9, 2, 3 },
+//             { 8, 4, 2, 4 },
+//             { 5, 2, 6, 7 }
+//         };
+
+//         int rows = array.GetLength(0);
+//         int cols = array.GetLength(1);
+
+//         int minSum = int.MaxValue; 
+//         int minSumRow = -1; 
+
+//         for (int i = 0; i < rows; i++)
+//         {
+//             int sum = 0;
+
+//             for (int j = 0; j < cols; j++)
+//             {
+//                 sum += array[i, j];
+//             }
+
+//             if (sum < minSum)
+//             {
+//                 minSum = sum;
+//                 minSumRow = i;
+//             }
+//         }
+
+//         Console.WriteLine("Строка с наименьшей суммой элементов: " + (minSumRow + 1)); // +1 для перевода от нулевого индекса к номеру строки
+
+//     }
+// }
+
+// Задача 58
 class Program
 {
     static void Main()
     {
-        int[,] array = 
-        {
-            { 1, 4, 7, 2 },
-            { 5, 9, 2, 3 },
-            { 8, 4, 2, 4 },
-            { 5, 2, 6, 7 }
+        int[,] matrix1 = {
+            { 2, 4 },
+            { 3, 2 }
         };
 
-        int rows = array.GetLength(0);
-        int cols = array.GetLength(1);
+        int[,] matrix2 = {
+            { 3, 4 },
+            { 3, 3 }
+        };
 
-        int minSum = int.MaxValue; 
-        int minSumRow = -1; 
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
 
-        for (int i = 0; i < rows; i++)
+        if (cols1 != rows2)
         {
-            int sum = 0;
+            Console.WriteLine("Невозможно выполнить умножение матриц.");
+            return;
+        }
 
-            for (int j = 0; j < cols; j++)
-            {
-                sum += array[i, j];
-            }
+        int[,] result = new int[rows1, cols2];
 
-            if (sum < minSum)
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
             {
-                minSum = sum;
-                minSumRow = i;
+                int sum = 0;
+                for (int k = 0; k < cols1; k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                result[i, j] = sum;
             }
         }
 
-        Console.WriteLine("Строка с наименьшей суммой элементов: " + (minSumRow + 1)); // +1 для перевода от нулевого индекса к номеру строки
-
+        Console.WriteLine("Результирующая матрица:");
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
